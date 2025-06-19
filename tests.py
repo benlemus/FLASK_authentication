6c004e4bffc8107b3825888bc88997d40cf2638f
benlemus <benlemus234@gmail.com>
2025-06-19 12:35:08 -0700

    started tests

--- tests.py
+++ tests.py
@@ -1,15 +1,24 @@
+from unittest import TestCase
 from app import app
-import unittest
+from models import db, User, Feedback
 
-class TestRoutes(unittest.TestCase):
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///auth_test_db'
+# app.config['SQLALCHEMY_ECHO'] = False
+# app.config['TESTING'] = True
+
+class TestRoutes(TestCase):
     def setUp(self):
-        pass
+        with app.app_context():
+            User.query.delete()
 
     def tearDown(self):
         pass
 
     def test_home_page(self):
-        pass
+        with app.test_client() as client:
+            res = client.get('/')
+
+            self.assertEqual(res.status_code, 200)
 
     def test_username_in_session(self):
         pass
