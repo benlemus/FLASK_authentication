b4871ceec0504ceb8e38a7246777f0ae7d4be7bd
Ben Lemus <benlemus@Bens-MBP-2.lan>
2025-06-18 17:58:40 -0700

    added tests file

---
+++ tests.py
@@ -0,0 +1,15 @@
+from app import app
+import unittest
+
+class TestRoutes(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_home_page(self):
+        pass
+
+    def test_username_in_session(self):
+        pass
